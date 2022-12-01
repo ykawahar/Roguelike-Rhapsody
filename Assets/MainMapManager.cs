829ad7170387d9a94e56e170a47e8115b0c7614e
Shalu Patel <shalu.patel1217@gmail.com>
2022-12-01 04:14:43 -0600

    transitioned map manager to MainMapManager
    - this wont be destroyed upon loading other scenes

--- Assets/MainMapManager.cs
+++ Assets/MainMapManager.cs
@@ -1,14 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class MainMapManager : MonoBehaviour
 {
-    public static MainMapManager Instance;
+    public static MainMapManager Instance; //creating a single instance with static values
+    public GameObject currentLevel;
+
+    public GameObject[] mapNodes;
+    public LinkedList<Sprite> currentMap = new LinkedList<Sprite>();
+
+    void Start()
+    {
+        mapNodes = GameObject.FindGameObjectsWithTag("MapNode");
+        foreach (GameObject mapNode in mapNodes) {
+            // if (mapNode.GetComponent<Image>().sprite.name == "swords") {
+            //     currentMap.AddLast(Resources.Load<Sprite>("Sprites/Icons/swords"));
+            // } else if (mapNode.GetComponent<Image>().sprite.name == "tribute") {
+            //     currentMap.AddLast(Resources.Load<Sprite>("Sprites/Icons/tribute"));
+            // } else {
+            //     //do nothing
+            // }
+        }
+        // Debug.Log(currentMap.First);
+    }
 
     private void Awake()
     {
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
+    // destroys duplicate instances
+    if (Instance != null)
+    {
+        Destroy(gameObject);
+        return;
+    }
+
+    Instance = this;
+    DontDestroyOnLoad(gameObject);
     }
 }

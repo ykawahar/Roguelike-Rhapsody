e9f6ed83421e6e6d8aa2d44e568739de59271dcc
Joseph Mena Lopez <jkmena3338@gmail.com>
2022-11-30 18:45:55 -0600

    basic player stats added, add/removve as needed

--- Assets/PlayerStats.cs
+++ Assets/PlayerStats.cs
@@ -5,4 +5,16 @@ using UnityEngine;
 public static class PlayerStats
 {
     public static string epithet = "";
+
+    public static int health = 50;
+
+    public static int maxHealth = 50;
+
+    public static int money = 50;
+
+    public static int enemiesSlain = 0;
+
+    public static int areasCleared = 0;
+
+    
 }

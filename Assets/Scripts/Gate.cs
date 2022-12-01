f8a9d79e2d471b01bb73951dbec8eaf200f4fda7
Shalu Patel <shalu.patel1217@gmail.com>
2022-11-30 23:24:38 -0600

    update gate.cs

--- Assets/Scripts/Gate.cs
+++ Assets/Scripts/Gate.cs
@@ -5,18 +5,23 @@ using UnityEngine.SceneManagement;
 using UnityEngine.Events;
 
 
+
 public class Gate : MonoBehaviour
 {
+    
+    Scene currentScene;
+    string sceneName;
+
     void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
-            SceneManager.LoadScene("CombatArea");
+            SceneManager.LoadScene("MapScene");
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentScene = SceneManager.GetActiveScene();
     }
 
     // Update is called once per frame

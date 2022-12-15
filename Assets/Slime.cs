e2687b39c8a4ff35c6267ec6551616c1c89cdbbc
ykawahar <60229462+ykawahar@users.noreply.github.com>
2022-12-14 22:16:33 -0600

    Slime Update
    Flubbing up with slime

--- Assets/Slime.cs
+++ Assets/Slime.cs
@@ -22,12 +22,15 @@ public class Slime : BasicEnemy
 
     protected override void Die(){
 
-        Vector3 newSpawnPos = new Vector3(transform.position.x - 1, transform.position.y+0.5f, transform.position.z);
+        Vector3 newSpawnPos = new Vector3(transform.position.x - 1, transform.position.y+1, transform.position.z);
 
-            GameObject clone = Instantiate(selfCopy, newSpawnPos, Quaternion.Euler(30,1,0));
-            // clone.transform.localScale = transform.localScale * 0.5f * Random.Range(0.8f, 1.2f);
-            
+            // GameObject clone = Instantiate(selfCopy, newSpawnPos, Quaternion.Euler(30,1,0));
+            float scaleFactor = Random.Range(0.8f, 1.2f);
+            GameObject clone = Instantiate(Resources.Load("Prefabs/Entity Prefabs/Slime Blue"), newSpawnPos, Quaternion.Euler(30,1,0)) as GameObject;
+            // instance.transform.localScale = transform.localScale * 0.5f * Random.Range(0.8f, 1.2f);
+            clone.transform.localScale = new Vector3(transform.localScale.x * 0.5f * scaleFactor,transform.localScale.y * 0.5f * scaleFactor, transform.localScale.z * 0.5f * scaleFactor);
+            // clone.transform.localScale = new Vector3(2,2,2);
         
-        // base.Die();
+            base.Die();
     }
 }
